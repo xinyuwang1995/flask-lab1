582d371521ee1768df98c9b9f35df60cf0e35e7c
xinyuwang1995 <wangxinyu19950915@gmail.com>
2019-11-08 00:45:50 +0800

    docker

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -6,5 +6,5 @@ else
     TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t wangxinyu1995/flask_ci_cd:$TAG .
-docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
+
 docker push wangxinyu1995/flask_ci_cd:$TAG
