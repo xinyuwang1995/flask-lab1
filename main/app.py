e8854c0ad512ebdda4f304813c02db2267405334
xinyuwang1995 <wangxinyu19950915@gmail.com>
2019-11-08 00:57:19 +0800

    docker

--- main/app.py
+++ main/app.py
@@ -4,10 +4,6 @@ from edit_distance import edit_dis
 #from flask_script import Manager,Server
 app = Flask(__name__)
 
-# 使用flask-script来运行
-#manager = Manager(app)
-# 开启debug
-#manager.add_command("runserver", Server(use_debugger=True))
 
 @app.route('/askme', methods=['GET', 'POST'])
 def validate():
