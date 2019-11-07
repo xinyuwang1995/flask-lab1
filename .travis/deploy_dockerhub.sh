6b0a386cbb3b5d617845e98cef92c15cba80646e
xinyuwang1995 <wangxinyu19950915@gmail.com>
2019-11-08 00:25:30 +0800

    docker

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-docker login --username $DOCKER_USER --password $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t flask-lab1:$TAG .
-docker tag flask-lab1:$TAG $DOCKER_REPO
-docker push $DOCKER_REPO
+docker build -f Dockerfile -t wangxinyu1995/flask_ci_cd:$TAG .
+
+docker push wangxinyu1995/flask_ci_cd:$TAG
